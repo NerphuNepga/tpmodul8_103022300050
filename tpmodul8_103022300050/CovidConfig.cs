9b2894df42d3e14577e1fcf755a4a6186b0f5448
M Arrayan <fighterfly9@gmail.com>
2025-04-13 17:50:06 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_103022300050/CovidConfig.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_103022300050
+{
+    internal class CovidConfig
+    {
+        public string satuan_suhu { get; set; }
+        public int batas_hari_demam { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+        
+        public CovidConfig() { }
+
+        public CovidConfig(string satuan_suhu, int batas_hari_demam, string pesan_ditolak, string pesan_diterima)
+        {
+            this.satuan_suhu = satuan_suhu;
+            this.batas_hari_demam = batas_hari_demam;
+            this.pesan_ditolak = pesan_ditolak;
+            this.pesan_diterima = pesan_diterima;
+        }
+    }
+    class UIConfig
+    {
+        public CovidConfig covidConfig;
+        public const string filePath = "C:\\benkyou, study me\\C#\\Konstruksi Perangkat Lunak\\tpmodul8_103022300050\\covid_config.json";
+        public UIConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+        private CovidConfig ReadConfigFile()
+        {
+            String configJsonData = File.ReadAllText(filePath);
+            covidConfig = JsonSerializer.Deserialize<CovidConfig>(configJsonData);
+            return covidConfig;
+        }
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            String jsonString = JsonSerializer.Serialize(covidConfig, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+        private void SetDefault()
+        {
+            covidConfig = new CovidConfig()
+            {
+                satuan_suhu = "celcius",
+                batas_hari_demam = 14,
+                pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+                pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini"
+            };
+        }
+        public void UbahSatuan()
+        {
+            if (covidConfig.satuan_suhu == "celcius")
+            {
+                covidConfig.satuan_suhu = "fahrenheit";
+            }
+            else
+            {
+                covidConfig.satuan_suhu = "celcius";
+            }
+            WriteNewConfigFile();
+            Console.WriteLine($"Satuan suhu telah diubah menjadi: {covidConfig.satuan_suhu}");
+        }
+    }
+}
--- tpmodul8_103022300050/Program.cs
+++ tpmodul8_103022300050/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul8_103022300050;
+
+UIConfig config = new UIConfig();
+
+config.UbahSatuan();
+
+try
+{
+    Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {config.covidConfig.satuan_suhu}: ");
+    double suhu = double.Parse(Console.ReadLine());
+
+    Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+    int hariDemam = int.Parse(Console.ReadLine());
+
+    bool suhuValid;
+    if (config.covidConfig.satuan_suhu == "celcius")
+    {
+        suhuValid = suhu >= 36.5 && suhu <= 37.5;
+    }
+    else
+    {
+        suhuValid = suhu >= 97.7 && suhu <= 99.5;
+    }
+
+    bool demamValid = hariDemam < config.covidConfig.batas_hari_demam;
+
+    if (suhuValid && demamValid)
+    {
+        Console.WriteLine(config.covidConfig.pesan_diterima);
+    }
+    else
+    {
+        Console.WriteLine(config.covidConfig.pesan_ditolak);
+    }
+}
+catch
+{
+    Console.WriteLine("Input tidak valid. Harap masukkan angka yang benar.");
+}
